c2765e5680d6d904ec9ad64925c83ac8c8b4775e
arvindayal <arvinddayal@gmail.com>
2014-01-16 17:36:06 -0700

    End of Day 1/16/2014

--- Metrics.js
+++ Metrics.js
@@ -1,3 +1,9 @@
+// Calcs and enters time spent on page
+function calctime() {	
+	var t = setTimeout("calctime()",1000);
+    document.getElementById("time").innerHTML="Seconds you spent viewing this page: " + t;
+}
+
 /**Shows/hides pop-up div containing metrics but clicking on button*/
 function showHide () {
 	var ele = document.getElementById("metric-popup");
@@ -9,16 +15,15 @@ function showHide () {
 	};
 }
 
-// Calcs and enters time spent on page
-function calctime() {	
-	var t = setTimeout("calctime()",1000);
-    document.getElementById("time").innerHTML="Seconds you spent viewing this page: " + t;
-}
-
 // Calculates and enters percent scrolled
 function scroll() {
     var p = ((document.body.scrollTop / document.body.scrollHeight) * 100);
     document.getElementById("scroll-per").innerHTML="You have viewed " + p + "% of this page.";
 }
 
-console.log(document.body.scrollTop)
+// Calcs distance scrolled
+function distance () {
+	var d = (pageYOffset);
+	document.getElementById("dist").innerHTML="You have scrolled " + d + " pixels.";
+	console.log(pageYOffset)
+}
